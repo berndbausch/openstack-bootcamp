d6b18fd8c5cadc77ffbde3c4b09143875869c167
Bernd Bausch <berndbausch@gmail.com>
2021-05-28 02:19:35 +0000

    various corrections, setup timetravel networks, openstack help file

---
+++ kolla3/setup-timetravel.sh
@@ -0,0 +1,16 @@
+# creates projects hadron and timetravel
+# creates user demo and gives it roles in those projects
+# creates timetravel-net and timetravel-router
+
+source /etc/kolla/admin_openrc.sh
+openstack project create hadron
+openstack project create timetravel
+openstack user create demo --password pw
+openstack role add --user demo --project hadron reader
+openstack role add --user demo --project timetravel member
+
+openstack network create timetravel-net --project timetravel
+openstack subnet create timetravel-subnet --project timetravel --network timetravel-net --subnet-range 10.0.0.0/24
+openstack router create timetravel-router --project timetravel
+openstack router set timetravel-router --external-gateway public1
+openstack router add subnet timetravel-router timetravel-subnet
