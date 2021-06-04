c2871cfd5cd583da77860064f9220624e5c8888b
Bernd Bausch <berndbausch@gmail.com>
2021-06-04 23:05:29 +0000

    added FORCE flag to prepare-swift scripts; corrected inventory file name in kolla-notes

--- kolla3/prepare-swift.sh
+++ kolla3/prepare-swift.sh
@@ -6,12 +6,17 @@
 
 ##### 1- create partitions on three disks, put XFS filesystems on them
 
+FORCE=""
+if [[ $1 = "-f" ]]
+then FORCE=-f
+fi
+
 index=0
 for i in b c d
 do 
     [ ! -e /dev/sd${i}1 ] && sudo parted -s /dev/sd$i mklabel gpt mkpart KOLLA_SWIFT_DATA 1 100%
     sleep 1
-    sudo mkfs -t xfs -L d${index} /dev/sd${i}1
+    sudo mkfs -t xfs $FORCE -L d${index} /dev/sd${i}1
     ((index++))
 done
 
