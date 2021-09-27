4ca3bd0469bcc5ed3d5afb579b9ec2dad5ff44f4
berndbausch <berndbausch@gmail.com>
2021-09-27 05:54:39 +0000

    update for single controller and Wallaby

--- kolla3/make-ubuntu.sh
+++ kolla3/make-ubuntu.sh
@@ -1,4 +1,4 @@
 virt-install --cpu host --name ubuntu --ram $((8*1024)) --vcpus 2 --graphics vnc,listen=0.0.0.0  \
              --network network=default --network network=provider    \
-             --cdrom /home/nobleprog/ubuntu-20.04.2-live-server-amd64.iso \
+             --cdrom /home/nobleprog/ubuntu-20.04.3-live-server-amd64.iso \
              --disk ubuntu-base.qcow2,size=50,bus=scsi
